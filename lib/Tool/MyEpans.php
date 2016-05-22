f98ab6809f2f4f07da83f39e08b4c98666d8e475
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-05-22 20:59:54 +0530

    CreateNew removed from here, now its under hook of order_placed and invoice_paid

--- lib/Tool/MyEpans.php
+++ lib/Tool/MyEpans.php
@@ -17,7 +17,7 @@ class Tool_MyEpans extends \xepan\cms\View_Tool {
 			return;
 		}
 
-		$customer = $this->add('xepan\commerce\Model_Customer');
+		$this->customer = $customer = $this->add('xepan\commerce\Model_Customer');
         $customer->loadLoggedIn();
 
         // check customer is loaded
@@ -25,47 +25,22 @@ class Tool_MyEpans extends \xepan\cms\View_Tool {
             $this->add('View_Info')->set('Customer account not found');
             return;            
         }
+
+		$this->showMyEpans();
 		
-		$new_btn  = $this->add('Button')->set('Create New Epan Web Site')->addClass('btn btn-primary btn-lg');
-		$grid = $this->add('Grid');
+	}
 
-		$vp = $this->add('VirtualPage');
-		$vp->set(function($p){
-			$new_epan = $this->add('xepan\epanservices\Model_Epan');
-
-			$form = $p->add('Form');
-			$form->setModel($new_epan,['epan_category_id','name']);
-
-			$installable_apps = $this->add('xepan\base\Model_Application')->addCondition('user_installable',true);
-			foreach ($installable_apps as $epan_app) {
-				$form->addField('CheckBox',$this->app->normalizeName($epan_app['name']),$epan_app['name']);
-			}
-
-			if($form->isSubmitted()){
-
-				$apps_selected=[];
-				foreach ($installable_apps as $epan_app) {
-					if($form[$this->app->normalizeName($epan_app['name'])]){
-						$apps_selected[$epan_app->id] = $epan_app['namespace'];
-					}
-				}
-				if($reply = $installable_apps->validateRequirements($apps_selected)){
-					if($reply instanceof \jQuery)
-						$form->js(null,$reply)->execute();
-					else
-						$form->js()->univ()->errorMessage($reply)->execute();
-				}
-				$form->save();
-				$grid->js(null,$grid->js()->univ()->cloaseDialog())->reload()->execute();
-			}
-		});
+	function showMyEpans(){
 
 		$this->app->addStyleSheet('jquery-ui');
-		$new_btn->js('click',$this->js()->univ()->frameURL('Create New Epan Web Site',$vp->getURL()));
+
+		$this->new_btn  = $this->add('Button')->set('Create New Epan Web Site')->addClass('btn btn-primary btn-lg');
+		$this->grid = $this->add('Grid');		
+		$this->new_btn->js('click',$this->js()->reload(['action'=>'createNew']));
 
 		$myEpans = $this->add('xepan\epanservices\Model_Epan');
-		$myEpans->addCondition('created_by_id',$this->app->auth->model->id);
+		$myEpans->addCondition('created_by_id',$this->customer->id);
 		
-		$grid->setModel($myEpans);
+		$this->grid->setModel($myEpans);
 	}
 }
