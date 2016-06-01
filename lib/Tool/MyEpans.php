a8da958cbf0d97db486c16b998a786d1e5be550d
vaibhav <vaibhavsharma38@gmail.com>
2016-06-01 18:57:59 +0530

    published and unpublished actions added on my epan grid

--- lib/Tool/MyEpans.php
+++ lib/Tool/MyEpans.php
@@ -35,15 +35,53 @@ class Tool_MyEpans extends \xepan\cms\View_Tool {
 		$myEpans = $this->add('xepan\epanservices\Model_Epan');
 		$myEpans->addCondition('created_by_id',$this->customer->id);
 		$this->grid->setModel($myEpans,['epan_category','xepan_template','created_by','name','status']);
-		$publish_button = $this->grid->addColumn('Button','Publish');
 
-		if($_GET['Publish']){
-			$new=$this->add('xepan\epanservices\Model_Epan');
-			$new->load($_GET['Publish']);
-			$new['published']=true;
-			$new->save();
-			$this->grid->js()->reload()->execute();
+		$this->grid->add('VirtualPage')
+       		 ->addColumn('Publish')
+       		 ->set(function($page){
+				$id = $_GET[$page->short_name.'_id'];
+				$new=$page->add('xepan\epanservices\Model_Epan');
+				$new->load($id);
+
+				
+			if($new['is_published']){
+				return $page->add('View_Info')->set('Already Published');
+			}
+
+			$form = $page->add('Form');
+			$form->addField('name');
+			$form->addSubmit('Submit');
+
+			if($form->isSubmitted()){
+				$new['name'] = $form['name'];
+				$new['is_published']=true;	
+				
+				$new->createFolder($new);
+				$new->userAndDatabaseCreate();
+				$new->save();	    		
+			}
+    	});
+
+
+       $this->grid->add('VirtualPage')
+       		 ->addColumn('UnPublish')
+       		 ->set(function($page){
+				$id = $_GET[$page->short_name.'_id'];
+				$new=$page->add('xepan\epanservices\Model_Epan');
+				$new->load($id);
+
+				
+			if(!$new['is_published']){
+				return $page->add('View_Info')->set('Already UnPublished');
+			}
 			
-		}
+			$form = $page->add('Form');
+			$form->addSubmit('Submit');
+			if($form->isSubmitted()){
+				$new['is_published']=null;	
+				$new->save();	    			
+			}
+			
+    	});		 
 	}
 }
