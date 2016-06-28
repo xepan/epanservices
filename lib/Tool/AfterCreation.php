6ea0cf029e5022be48db3993cc243c5d36e33581
vaibhav <vaibhavsharma38@gmail.com>
2016-06-28 17:39:33 +0530

    redirected to greetings page

--- lib/Tool/AfterCreation.php
+++ lib/Tool/AfterCreation.php
@@ -8,6 +8,12 @@ class Tool_AfterCreation extends \xepan\cms\View_Tool {
 
 	function init(){
 		parent::init();		
+		
+		$admin_url = "http://www.".$_GET['epan_name']."epan.in/admin";
+		$website_url = "http://www.".$_GET['epan_name']."epan.in";
+
+		$this->template->trySet('admin',$admin_url);
+		$this->template->trySet('website',$website_url);
 	}
 
 	function defaultTemplate(){
