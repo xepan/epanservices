e334e13b5240b2c800fdd68f465c58044b931da5
vijay mali <guddavijay@gmail.com>
2016-07-21 16:02:45 +0530

    db version sql  released

---
+++ lib/Model/DbVersion.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * This model traverses pages of your project to look for test-cases.
+ *
+ * Test-files must implement a class descending from Page_Tester, refer
+ * to that class for more info. 
+ */
+
+namespace xepan\epanservices;
+
+class Model_DbVersion extends \Model {
+    
+    public $dir='dbversion';
+    public $namespace=null;
+    public $max_count=0;
+    public $path="";
+    function init(){
+        parent::init();
+
+        $this->addField('name')->defaultValue();
+        $this->addField('code')->type('text');
+
+        /**
+         * This model automatically sets its source by traversing 
+         * and searching for suitable files
+         */
+        $path = $this->path = $this->api->pathfinder->base_location->base_path.'/../vendor/'.str_replace("\\","/",$this->namespace)."/".$this->dir;
+        $p = scandir($path); 
+        unset($p[0]);
+        unset($p[1]);
+
+        asort($p);
+        $i=2;
+        
+        foreach ($p as $file) {
+            $temp = explode(".", explode("-", $file)[1]);
+            if(is_array($temp) and isset($temp[0])){
+                if($this->max_count < $temp[0] )
+                    $this->max_count = $temp[0];
+            }
+
+            if(strpos($file, ".sql")===false) unset($p[$i]);
+            $i++;
+        }
+
+        asort($p);
+        $this->setSource('Array',$p);
+
+        $this->addHook('beforeDelete',$this);
+
+        return $this;
+    }
+
+    function beforeDelete($m){
+        // throw new \Exception($this->id, 1);
+        if(file_exists($this->path.'/'.$this['name'])){
+            unlink($this->path.'/'.$this['name']);
+        }
+    }
+}
