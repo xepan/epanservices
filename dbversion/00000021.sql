7e1276170e27e876f4ff32cf98e35635f5f2ae25
dkanojia <dkanojia93@gmail.com>
2016-09-19 12:58:30 +0530

    new dbversion

--- dbversion/00000021.sql
+++ dbversion/00000021.sql
@@ -1,9 +1,11 @@
 CREATE TABLE `bar_code_reader` (
-`id`  int(11) NOT NULL AUTO_INCREMENT ,
-`name`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+`id`  int(11) NOT NULL ,
+`name`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
 `is_used`  tinyint(4) NULL DEFAULT NULL ,
 `related_document_id`  int(11) NULL DEFAULT NULL ,
-`related_document_type`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL ,
+`related_document_type`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
+`status`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
+`type`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
 PRIMARY KEY (`id`)
 )
 ENGINE=InnoDB
