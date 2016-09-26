e46fcfbb978b408d2415deb9a60ee5994acdf329
vijay mali <guddavijay@gmail.com>
2016-09-26 12:03:47 +0530

    employee related table deleted

---
+++ dbversion/00000031.sql
@@ -0,0 +1,1 @@
+DROP TABLE `employee_allowed_leave`, `employee_attandance`, `employee_transaction`, `employee_transaction_row`, `leave`, `leave_template`, `official_holidays`, `payment_and_deduction`, `salary_template`;
