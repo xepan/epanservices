fc562342c1ec9efc4ae5cb4eaf9f7a9862dc105e
vaibhav <vaibhavsharma38@gmail.com>
2016-09-30 17:48:00 +0530

    new fields added

---
+++ dbversion/00000040.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `custom_form` DROP COLUMN `created_at`;
+ALTER TABLE `custom_form` DROP COLUMN `created_by_id`;
+ALTER TABLE `custom_form` DROP COLUMN `type`;
+ALTER TABLE `custom_form` DROP COLUMN `status`;
