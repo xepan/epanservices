f3083a5937f570467bd10ad4a76fe504c7fd95d0
sinharakesh <rksinha.btech@gmail.com>
2016-12-01 16:23:09 +0530

    composition item few fields added

---
+++ dbversion/00000101.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `item_department_consumption` ADD `item_customfield_id` INT(11) NULL;
+ALTER TABLE `item_department_consumption` ADD `item_customfield_asso_id` INT(11) NULL;
+ALTER TABLE `item_department_consumption` ADD `item_customfield_value_id` INT(11) NULL;
+ALTER TABLE `item_department_consumption` ADD `item_customfield_name` VARCHAR(255) NULL;
+ALTER TABLE `item_department_consumption` ADD `item_customfield_value_name` VARCHAR(255) NULL;
