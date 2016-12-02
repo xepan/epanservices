ceffab2599ea00a88c38a668bd68c7e455ea6288
sinharakesh <rksinha.btech@gmail.com>
2016-12-02 11:22:58 +0530

    consumption table drop column of constrain

---
+++ dbversion/00000102.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `item_department_consumption`
+  DROP `item_customfield_id`,
+  DROP `item_customfield_asso_id`,
+  DROP `item_customfield_value_id`,
+  DROP `item_customfield_name`,
+  DROP `item_customfield_value_name`;
