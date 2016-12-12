f95bebd82862816d8c03723ae0e87926da68422a
sinharakesh <rksinha.btech@gmail.com>
2016-12-12 11:39:51 +0530

    salary table column added

---
+++ dbversion/00000113.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `salary` ADD `add_deduction` VARCHAR(255);
+ALTER TABLE `salary` ADD `default_value` VARCHAR(255);
+ALTER TABLE `salary` ADD `order` INT;
+ALTER TABLE `salary` DROP `add_deducat`;
