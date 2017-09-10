a37b18e86d3cc9d48bf6b161e3478b1b9c27a8b4
vijay mali <guddavijay@gmail.com>
2017-09-10 19:03:47 +0530

    added page meta info

---
+++ dbversion/00000188.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `webpage` ADD COLUMN `page_title`  text DEFAULT NULL;
+ALTER TABLE `webpage` ADD COLUMN `meta_kewords`  text DEFAULT NULL;
+ALTER TABLE `webpage` ADD COLUMN `meta_description`  text DEFAULT NULL;
+ALTER TABLE `webpage` ADD COLUMN `after_body_code`  text DEFAULT NULL;
