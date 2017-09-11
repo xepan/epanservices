5cdcfd6c56ec8629afa23b0f7b0baa8c9be2e989
sinharakesh <rksinha.btech@gmail.com>
2017-09-11 12:11:42 +0530

    toool theme option added

--- lib/Tool/Theme.php
+++ lib/Tool/Theme.php
@@ -5,7 +5,11 @@ namespace xepan\epanservices;
 
 class Tool_Theme extends \xepan\cms\View_Tool{
 	public $options = [
-		'dashboard_page' => "customer-dashboard"
+			'dashboard_page' => "customer-dashboard",
+			'show_progress_bar'=>1,
+			'show_preview_button'=>1,
+			'show_applynow_button'=>1,
+			'show_search'=>1
 		];
 
 	function init(){
@@ -15,15 +19,22 @@ class Tool_Theme extends \xepan\cms\View_Tool{
 
 		$epan_detail = $this->app->recall('newepan');
 
-		$this->add('xepan\epanservices\View_ProgressBar',['active_step'=>3]);
-		
+		if($this->options['show_progress_bar']){
+			$this->add('xepan\epanservices\View_ProgressBar',['active_step'=>($_GET['active_step']?:3)]);
+		}
+				
 		$this->add('xepan\cms\View_Theme',[
 						'apply_theme_on_website'=>$epan_detail['epan_name'],
 						'apply_theme_on_website_id'=>$epan_detail['epan_id'],
-						'dashboard_page'=>$this->options['dashboard_page']
+						'dashboard_page'=>$this->options['dashboard_page'],
+						'show_preview_button'=>$this->options['show_preview_button'],
+						'show_applynow_button'=>$this->options['show_applynow_button'],
+						'show_search'=>$this->options['show_search']
 					]);
 
-		$this->add('View',null,'skip')->setElement('a')->setAttr('href',$this->app->url($this->options['dashboard_page']))->set('Skip Step');
+		if($this->options['show_progress_bar']){
+			$this->add('View',null,'skip')->setElement('a')->setAttr('href',$this->app->url($this->options['dashboard_page']))->set('Skip Step');
+		}
 	}
 
 	function defaultTemplate(){
