e38ca62df3abde73de3c2c76dc236fc4ad4ed7b5
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-10-24 08:59:19 +0530

    edit any remote epan from admin

--- page/epans.php
+++ page/epans.php
@@ -16,10 +16,41 @@ class page_epans extends \xepan\base\Page {
 
 	function init(){
 		parent::init();
+
+		if(($live_edit_id = $_GET['live_edit']) OR ($live_edit_id = $_GET['admin_login'])){
+			$epan = $this->add('xepan\base\Model_Epan')->tryLoadBy('id',$live_edit_id);
+			$token = md5(uniqid());
+			$this->add('xepan\epanservices\Controller_RemoteEpan')
+				->setEpan($epan)
+				->do(function($app)use($token){
+					$app->add('xepan\base\Model_User')
+						->tryLoadAny()
+						->set('access_token',$token)
+						->set('access_token_expiry',date('Y-m-d H:i:s',strtotime($app->now.' +10 seconds')))
+						->save();
+				});
+			$this->url = $url = "{$_SERVER['HTTP_HOST']}";        
+	        $this->protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
+	        $this->domain = $domain = str_replace('www.','',$this->app->extract_domain($url))?:'www';
+	        $this->sub_domain = $sub_domain = str_replace('www.','',$this->app->extract_subdomains($url))?:'www';
+	        if($_GET['live_edit'])
+				$this->js()->univ()->newWindow($this->app->url($this->protocol.$epan['name'].".".$this->domain.str_replace('/admin/','',(string)$this->app->pathfinder->base_location->base_url),['access_token'=>$token]),'LiveEdit')->execute();
+						
+	        if($_GET['admin_login'])
+				$this->js()->univ()->newWindow($this->app->url($this->protocol.$epan['name'].".".$this->domain.$this->app->pathfinder->base_location->base_url."admin"),'adminpanel')->execute();
+
+		}
+
 		
-		$crud = $this->add('xepan\hr\CRUD',null,null,['view\epans']);
-		$crud->setModel('xepan\epanservices\Epan')->addCondition([['is_template',false],['is_template',null]]);
+		$crud = $this->add('xepan\hr\CRUD',null,null,null);
+		$crud->setModel('xepan\epanservices\Epan',['name','created_by','created_at','expiry_date','status'])
+			->addCondition([['is_template',false],['is_template',null]])
+			->setOrder('created_at','desc')
+			;
 		
 		$crud->grid->addQuickSearch(['name']);
+
+		$crud->grid->addColumn('Button','live_edit',['descr'=>'Frontend Edit','button_class'=>'btn btn-primary']);
+		$crud->grid->removeColumn('status');
 	}
 }
