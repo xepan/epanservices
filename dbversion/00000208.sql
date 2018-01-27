fdc75642bce448827366496ebea78e28496132f4
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-01-27 12:05:50 +0530

    add is_read index also

--- dbversion/00000208.sql
+++ dbversion/00000208.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `communication_read_emails` ADD INDEX contact_communication USING BTREE (`contact_id`,`communication_id`) comment '';
+ALTER TABLE `communication_read_emails` ADD INDEX is_read USING BTREE (`is_read`) comment '';
