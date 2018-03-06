daf61e221f86f6f2cf4d90cc65ce521ed7859789
monikapokra <monikagupta495@gmail.com>
2018-03-06 11:50:26 +0530

    blog_category_group model added

---
+++ dbversion/00000218.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `blog_category_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `categories` text ,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
